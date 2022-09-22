0f58227dcd8893c85c081c3424f0f10a2d3fb7b4
okhan1998 <whwlgns111@gmail.com>
2022-09-22 16:52:14 +0900

    feat: 리액트 라우터 셋팅 및 리액트 폴더구조 셋팅

--- najuha-fronted/src/App.js
+++ najuha-fronted/src/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset' // 리액트에서는 styled-reset을 통해 모든 스타일을 reset 할 수 있어요.
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const GlobalStyle = createGlobalStyle`
   ${reset}`
@@ -9,7 +10,12 @@ function App() {
   return (
     <React.Fragment>
     <GlobalStyle />  
+    <BrowserRouter>
+      <Routes>
+        <Route path = '/' element={<h1>hello</h1>} />
+      </Routes>
 
+    </BrowserRouter>
     </React.Fragment>
   );
 }
