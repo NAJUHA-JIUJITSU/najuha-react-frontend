a011ccd7b2fa67749cdafdd25348d96609dc2010
okhan1998 <whwlgns111@gmail.com>
2022-10-03 01:12:12 +0900

    feat: 대회일정 대회카드 디자인 완료

---
+++ najuha-fronted/src/components/Competitionlist.js
@@ -0,0 +1,231 @@
+import React from 'react'
+import './competitionlist.css'
+
+function Competitionlist() {
+
+
+
+
+
+
+  return (
+    <div className='competition-schedule-wrapper'>
+        <div className='searchzone'>
+        searchzone
+        </div>
+        <div className='competition-list'>
+            <ul class='competition-row'>
+                <li class='competition-col'>
+                    <div class='each-competition'>
+                        <div class='each-competition-top'>
+                            <div class='each-competition-top-date'>
+                                <h1>08.27<span>(월)</span></h1>
+                            </div>
+                            <div class='each-competition-top-location'>
+                                <div class='each-competition-top-location-tag'> {/*display:flex써서 세로 가운데 정렬하려고 클래스하나 더 넣은거임 */}
+                                    <h2>예거스 챔피원쉽 로컬대회 송도 오픈</h2><br/>
+                                    <h3>서울,KBS88제2체육관</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='each-competition-bottom'>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.13(수)</h4>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.19(화)</h4>
+                            <div class='each-competition-bottom-buttons'>
+                                <button>세부정보</button>
+                                <button>신청</button>
+                            </div>
+                        </div> 
+                    </div>
+                </li>
+                <li class='competition-col'>
+                    <div class='each-competition'>
+                        <div class='each-competition-top'>
+                            <div class='each-competition-top-date'>
+                                <h1>08.27<span>(월)</span></h1>
+                            </div>
+                            <div class='each-competition-top-location'>
+                                <div class='each-competition-top-location-tag'> {/*display:flex써서 세로 가운데 정렬하려고 클래스하나 더 넣은거임 */}
+                                    <h2>예거스 챔피원쉽 로컬대회 송도 오픈</h2><br/>
+                                    <h3>서울,KBS88제2체육관</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='each-competition-bottom'>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.13(수)</h4>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.19(화)</h4>
+                            <div class='each-competition-bottom-buttons'>
+                                <button>세부정보</button>
+                                <button>신청</button>
+                            </div>
+                        </div> 
+                    </div>
+                </li>
+                <li class='competition-col'>
+                    <div class='each-competition'>
+                        <div class='each-competition-top'>
+                            <div class='each-competition-top-date'>
+                                <h1>08.27<span>(월)</span></h1>
+                            </div>
+                            <div class='each-competition-top-location'>
+                                <div class='each-competition-top-location-tag'> {/*display:flex써서 세로 가운데 정렬하려고 클래스하나 더 넣은거임 */}
+                                    <h2>예거스 챔피원쉽 로컬대회 송도 오픈</h2><br/>
+                                    <h3>서울,KBS88제2체육관</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='each-competition-bottom'>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.13(수)</h4>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.19(화)</h4>
+                            <div class='each-competition-bottom-buttons'>
+                                <button>세부정보</button>
+                                <button>신청</button>
+                            </div>
+                        </div> 
+                    </div>
+                </li>
+                <li class='competition-col'>
+                    <div class='each-competition'>
+                        <div class='each-competition-top'>
+                            <div class='each-competition-top-date'>
+                                <h1>08.27<span>(월)</span></h1>
+                            </div>
+                            <div class='each-competition-top-location'>
+                                <div class='each-competition-top-location-tag'> {/*display:flex써서 세로 가운데 정렬하려고 클래스하나 더 넣은거임 */}
+                                    <h2>예거스 챔피원쉽 로컬대회 송도 오픈</h2><br/>
+                                    <h3>서울,KBS88제2체육관</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='each-competition-bottom'>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.13(수)</h4>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.19(화)</h4>
+                            <div class='each-competition-bottom-buttons'>
+                                <button>세부정보</button>
+                                <button>신청</button>
+                            </div>
+                        </div> 
+                    </div>
+                </li>
+                <li class='competition-col'>
+                    <div class='each-competition'>
+                        <div class='each-competition-top'>
+                            <div class='each-competition-top-date'>
+                                <h1>08.27<span>(월)</span></h1>
+                            </div>
+                            <div class='each-competition-top-location'>
+                                <div class='each-competition-top-location-tag'> {/*display:flex써서 세로 가운데 정렬하려고 클래스하나 더 넣은거임 */}
+                                    <h2>예거스 챔피원쉽 로컬대회 송도 오픈</h2><br/>
+                                    <h3>서울,KBS88제2체육관</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='each-competition-bottom'>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.13(수)</h4>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.19(화)</h4>
+                            <div class='each-competition-bottom-buttons'>
+                                <button>세부정보</button>
+                                <button>신청</button>
+                            </div>
+                        </div> 
+                    </div>
+                </li>
+                <li class='competition-col'>
+                    <div class='each-competition'>
+                        <div class='each-competition-top'>
+                            <div class='each-competition-top-date'>
+                                <h1>08.27<span>(월)</span></h1>
+                            </div>
+                            <div class='each-competition-top-location'>
+                                <div class='each-competition-top-location-tag'> {/*display:flex써서 세로 가운데 정렬하려고 클래스하나 더 넣은거임 */}
+                                    <h2>예거스 챔피원쉽 로컬대회 송도 오픈</h2><br/>
+                                    <h3>서울,KBS88제2체육관</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='each-competition-bottom'>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.13(수)</h4>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.19(화)</h4>
+                            <div class='each-competition-bottom-buttons'>
+                                <button>세부정보</button>
+                                <button>신청</button>
+                            </div>
+                        </div> 
+                    </div>
+                </li>
+                <li class='competition-col'>
+                    <div class='each-competition'>
+                        <div class='each-competition-top'>
+                            <div class='each-competition-top-date'>
+                                <h1>08.27<span>(월)</span></h1>
+                            </div>
+                            <div class='each-competition-top-location'>
+                                <div class='each-competition-top-location-tag'> {/*display:flex써서 세로 가운데 정렬하려고 클래스하나 더 넣은거임 */}
+                                    <h2>예거스 챔피원쉽 로컬대회 송도 오픈</h2><br/>
+                                    <h3>서울,KBS88제2체육관</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='each-competition-bottom'>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.13(수)</h4>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.19(화)</h4>
+                            <div class='each-competition-bottom-buttons'>
+                                <button>세부정보</button>
+                                <button>신청</button>
+                            </div>
+                        </div> 
+                    </div>
+                </li>
+                <li class='competition-col'>
+                    <div class='each-competition'>
+                        <div class='each-competition-top'>
+                            <div class='each-competition-top-date'>
+                                <h1>08.27<span>(월)</span></h1>
+                            </div>
+                            <div class='each-competition-top-location'>
+                                <div class='each-competition-top-location-tag'> {/*display:flex써서 세로 가운데 정렬하려고 클래스하나 더 넣은거임 */}
+                                    <h2>예거스 챔피원쉽 로컬대회 송도 오픈</h2><br/>
+                                    <h3>서울,KBS88제2체육관</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='each-competition-bottom'>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.13(수)</h4>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.19(화)</h4>
+                            <div class='each-competition-bottom-buttons'>
+                                <button>세부정보</button>
+                                <button>신청</button>
+                            </div>
+                        </div> 
+                    </div>
+                </li>
+                <li class='competition-col'>
+                    <div class='each-competition'>
+                        <div class='each-competition-top'>
+                            <div class='each-competition-top-date'>
+                                <h1>08.27<span>(월)</span></h1>
+                            </div>
+                            <div class='each-competition-top-location'>
+                                <div class='each-competition-top-location-tag'> {/*display:flex써서 세로 가운데 정렬하려고 클래스하나 더 넣은거임 */}
+                                    <h2>예거스 챔피원쉽 로컬대회 송도 오픈</h2><br/>
+                                    <h3>서울,KBS88제2체육관</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='each-competition-bottom'>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.13(수)</h4>
+                            <h4><img src='Assets/타이머.svg' alt='신청기간아이콘'/>08.19(화)</h4>
+                            <div class='each-competition-bottom-buttons'>
+                                <button>세부정보</button>
+                                <button>신청</button>
+                            </div>
+                        </div> 
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Competitionlist
--- najuha-fronted/src/pages/CompetitionSchedule.js
+++ najuha-fronted/src/pages/CompetitionSchedule.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Competitionlist from '../components/Competitionlist'
 
 function CompetitionSchedule() {
   return (
-    <>
+    <div class=''>
         <Navbar />
+        <Competitionlist />
         <Footer />
-    </>
+    </div>
   )
 }
 
