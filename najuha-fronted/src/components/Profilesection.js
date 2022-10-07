7dc81977974cb5196f20b41e37009da9790289ec
yeonah98 <dbdusdkaa@naver.com>
2022-10-07 19:36:11 +0900

    Feat: Add profilesection_myCompetitionList

--- najuha-fronted/src/components/Profilesection.js
+++ najuha-fronted/src/components/Profilesection.js
@@ -20,17 +20,17 @@ function Profilesection() {
                 </div>
             </div>
             <div className='Profilesection_competitionCount'>
-                <div className='Profilesection_competitionCount-box now'>
-                    <di className='Profilesection_competitionCount-box-center'>
+                <div className='Profilesection_competitionCount-box nowCnt'>
+                    <div className='Profilesection_competitionCount-box-center'>
                         <p className='Profilesection_competitionCount-box-num'>3</p>
-                        <p>실시간 대회</p>
-                    </di>
+                        <p>실시간 대회신청</p>
+                    </div>
                 </div>
-                <div className='Profilesection_competitionCount-box total'>
-                    <di className='Profilesection_competitionCount-box-center'>
+                <div className='Profilesection_competitionCount-box totalCnt'>
+                    <div className='Profilesection_competitionCount-box-center'>
                         <p className='Profilesection_competitionCount-box-num'>24</p>
                         <p>총 대회신청</p>
-                    </di>
+                    </div>
                 </div>
             </div>
             <div className='Profilesection_information'>
@@ -43,7 +43,107 @@ function Profilesection() {
                 </li>
             </div>
         </section>
-        <section className='Profilesection_right'></section>
+        <section className='Profilesection_right'>
+            <h2>대회신청목록</h2>
+            <div className='Profilesection_myCompetitionList nowList'>
+                <h3>실시간 대회신청</h3>
+                <p>전체보기<img src='Assets/arrow_right.svg' alt='오른쪽 화살표'></img></p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Location</th>
+                            <th>Champitonship</th>
+                            <th>참가부</th>
+                            <th>벨트</th>
+                            <th>체급</th>
+                            <th>결제내역</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>09.27</td>
+                            <td>서울</td>
+                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
+                            <td>마스터1</td>
+                            <td>블루</td>
+                            <td>-76.0kg</td>
+                            <td>결제완료</td>
+                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
+                        </tr>
+                        <tr>
+                            <td>09.27</td>
+                            <td>서울</td>
+                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
+                            <td>마스터1</td>
+                            <td>블루</td>
+                            <td>-76.0kg</td>
+                            <td>결제완료</td>
+                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
+                        </tr>
+                        <tr>
+                            <td>09.27</td>
+                            <td>서울</td>
+                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
+                            <td>마스터1</td>
+                            <td>블루</td>
+                            <td>-76.0kg</td>
+                            <td>결제완료</td>
+                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className='Profilesection_myCompetitionList totlaList'>
+                <h3>총 대회신청</h3>
+                <p>전체보기<img src='Assets/arrow_right.svg' alt='오른쪽 화살표'></img></p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Location</th>
+                            <th>Champitonship</th>
+                            <th>참가부</th>
+                            <th>벨트</th>
+                            <th>체급</th>
+                            <th>결제내역</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>09.27</td>
+                            <td>서울</td>
+                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
+                            <td>마스터1</td>
+                            <td>블루</td>
+                            <td>-76.0kg</td>
+                            <td>결제완료</td>
+                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
+                        </tr>
+                        <tr>
+                            <td>09.27</td>
+                            <td>서울</td>
+                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
+                            <td>마스터1</td>
+                            <td>블루</td>
+                            <td>-76.0kg</td>
+                            <td>결제완료</td>
+                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
+                        </tr>
+                        <tr>
+                            <td>09.27</td>
+                            <td>서울</td>
+                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
+                            <td>마스터1</td>
+                            <td>블루</td>
+                            <td>-76.0kg</td>
+                            <td>결제완료</td>
+                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
         </div>
     )
 
