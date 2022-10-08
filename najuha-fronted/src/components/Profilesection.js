33d3f9380d01e5f99f7ea1bd4ad01d98d4e0df6d
yeonah98 <dbdusdkaa@naver.com>
2022-10-08 20:51:00 +0900

    Feat: Add Profilepage  반응형

--- najuha-fronted/src/components/Profilesection.js
+++ najuha-fronted/src/components/Profilesection.js
@@ -1,11 +1,172 @@
 import React from 'react'
 import {useState, useEffect} from 'react';
 import './profilesection.css'
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom'
 
 function Profilesection() {
-
     const [username, setUsername] = useState("나주하");
+    const [competitionApplications, setCompetitionApplications] = useState([]);
+
+    async function getUsers() {
+        axios.get(`${process.env.REACT_APP_BACK_END_API}/users`,
+        {
+            headers: {
+                'x-access-token':  process.env.REACT_APP_BACK_END_TOKEN
+            }
+        })
+        .then((res) => {
+            setUsername(res.data.result.userInfo.fullName);
+            console.log(res.data.message);
+        })
+        .catch((err) => {
+            console.log(err);
+            console.log(err.response.status);
+            console.log(err.response.data.message);
+        })
+        return ;
+    }
+
+    async function getCompetitionApplication() {
+        axios.get(`${process.env.REACT_APP_BACK_END_API}/users/competitionApplications`,
+        {
+            headers: {
+                'x-access-token':  process.env.REACT_APP_BACK_END_TOKEN
+            }
+        })
+        .then((res) => {
+            setCompetitionApplications(res.data.result);
+            console.log(res.data.message);
+        })
+        .catch((err) => {
+            console.log(err);
+            console.log(err.response.status);
+            console.log(err.response.data.message);
+        })
+        return ;
+    }
+
+    function applicationParsing(application){
+        let doreOpen = application.Competition.doreOpen.substr(5,5).replace('-','.');
+        let locations = application.Competition.location.split(' ');
+        let location = locations[0]
+        let title = (application.Competition.title.length > 24) ? application.Competition.title.substr(0, 14) + '...' : application.Competition.title;
+        let divisionName = application.divisionName;
+        let belt = application.belt.charAt(0).toUpperCase() + application.belt.slice(1);
+        let uniform = (application.uniform = "gi") ? '기-' : '노기-';
+        let weight = application.weight + 'kg';
+        let isPayment = application.isPayment ? '결제완료' : '미결제';
+       
+        return {
+            'doreOpen': doreOpen,
+            'location': location,
+            'title': title,
+            'divisionName': uniform + divisionName,
+            'belt': belt,
+            'weight': weight,
+            'isPayment': isPayment,
+        }
+    }
+
+    function renderCompetition(){
+        return competitionApplications.map((application) => {
+            let curApplication = applicationParsing(application);
+            let today = new Date();
+            if( today > new Date(application.Competition.doreOpen)) {
+                return ;
+            }
+            return(
+                <tbody>
+                        <tr className='Profilesection_PcTable'>
+                            <td>{curApplication.doreOpen}</td>
+                            <td>{curApplication.location}</td>
+                            <td>{curApplication.title}</td>
+                            <td>{curApplication.divisionName}</td>
+                            <td>{curApplication.belt}</td>
+                            <td>{curApplication.weight}</td>
+                            <td>{curApplication.isPayment}</td>
+                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
+                        </tr>
+                        <tr className='Profilesection_MobileTable'>
+                            <td><div id='Profilesection_tableDate'><p>{curApplication.doreOpen}</p></div></td>
+                            <td>{curApplication.location}</td>
+                            <td>{curApplication.title}</td>
+                            <td>{curApplication.divisionName}</td>
+                        </tr>
+                        <tr className='Profilesection_MobileTable Profilesection_odd'>
+                            <td>{curApplication.belt}</td>
+                            <td>{curApplication.weight}</td>
+                            <td>{curApplication.isPayment}</td>
+                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
+                        </tr>
+                </tbody>
+            )
+        })
+    }
+
+    function renderLastCompetition(){
+        return competitionApplications.map((application) => {
+            let curApplication = applicationParsing(application);
+            let today = new Date();
+            if( today < new Date(application.Competition.doreOpen)) {
+                return ;
+            }
+            return(
+                <tbody>
+                        <tr className='Profilesection_PcTable'>
+                            <td>{curApplication.doreOpen}</td>
+                            <td>{curApplication.location}</td>
+                            <td>{curApplication.title}</td>
+                            <td>{curApplication.divisionName}</td>
+                            <td>{curApplication.belt}</td>
+                            <td>{curApplication.weight}</td>
+                            <td>{curApplication.isPayment}</td>
+                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
+                        </tr>
+                        <tr className='Profilesection_MobileTable'>
+                            <td><div id='Profilesection_tableDate'><p>{curApplication.doreOpen}</p></div></td>
+                            <td>{curApplication.location}</td>
+                            <td>{curApplication.title}</td>
+                            <td>{curApplication.divisionName}</td>
+                        </tr>
+                        <tr className='Profilesection_MobileTable Profilesection_odd'>
+                            <td>{curApplication.belt}</td>
+                            <td>{curApplication.weight}</td>
+                            <td>{curApplication.isPayment}</td>
+                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
+                        </tr>
+                </tbody>
+            )
+
+        })
+    }
+
+    function renderCompetitonNowCount() {
+        let nowCnt = 0;
+        competitionApplications.map((application) => {
+            let competitionDate = new Date(application.Competition.doreOpen);
+            let today = new Date();
+            if( today <= competitionDate) {
+                nowCnt++;
+            }
+        })
+        return (
+            <p className='Profilesection_competitionCount-box-num'>{nowCnt}</p>
+        )
+    }
+
+    function renderCompetitonTotalCount() {
+        let totalCnt = competitionApplications.length
+        return (
+         <p className='Profilesection_competitionCount-box-num'>{totalCnt}</p>
+        )
+    }
+
+    useEffect(() => {
+        getUsers();
+        getCompetitionApplication();
+    }, [])
+
 
     return (
         <div className='Profilesection_wrapper'>
@@ -22,13 +183,13 @@ function Profilesection() {
             <div className='Profilesection_competitionCount'>
                 <div className='Profilesection_competitionCount-box nowCnt'>
                     <div className='Profilesection_competitionCount-box-center'>
-                        <p className='Profilesection_competitionCount-box-num'>3</p>
+                        {renderCompetitonNowCount()}
                         <p>실시간 대회신청</p>
                     </div>
                 </div>
                 <div className='Profilesection_competitionCount-box totalCnt'>
                     <div className='Profilesection_competitionCount-box-center'>
-                        <p className='Profilesection_competitionCount-box-num'>24</p>
+                        {renderCompetitonTotalCount()}
                         <p>총 대회신청</p>
                     </div>
                 </div>
@@ -46,10 +207,12 @@ function Profilesection() {
         <section className='Profilesection_right'>
             <h2>대회신청목록</h2>
             <div className='Profilesection_myCompetitionList nowList'>
-                <h3>실시간 대회신청</h3>
-                <p>전체보기<img src='Assets/arrow_right.svg' alt='오른쪽 화살표'></img></p>
+                <div className='Profilesection_tableName'>
+                    <h3>실시간 대회신청</h3>
+                    <p>전체보기<img src='Assets/arrow_right.svg' alt='오른쪽 화살표'></img></p>
+                </div>
                 <table>
-                    <thead>
+                    <thead className='Profilesection_thead'>
                         <tr>
                             <th>Date</th>
                             <th>Location</th>
@@ -60,45 +223,14 @@ function Profilesection() {
                             <th>결제내역</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <td>09.27</td>
-                            <td>서울</td>
-                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
-                            <td>마스터1</td>
-                            <td>블루</td>
-                            <td>-76.0kg</td>
-                            <td>결제완료</td>
-                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
-                        </tr>
-                        <tr>
-                            <td>09.27</td>
-                            <td>서울</td>
-                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
-                            <td>마스터1</td>
-                            <td>블루</td>
-                            <td>-76.0kg</td>
-                            <td>결제완료</td>
-                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
-                        </tr>
-                        <tr>
-                            <td>09.27</td>
-                            <td>서울</td>
-                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
-                            <td>마스터1</td>
-                            <td>블루</td>
-                            <td>-76.0kg</td>
-                            <td>결제완료</td>
-                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
-                        </tr>
-                    </tbody>
+                    {renderCompetition()}
                 </table>
             </div>
             <div className='Profilesection_myCompetitionList totlaList'>
                 <h3>총 대회신청</h3>
                 <p>전체보기<img src='Assets/arrow_right.svg' alt='오른쪽 화살표'></img></p>
                 <table>
-                    <thead>
+                    <thead className='Profilesection_thead'>
                         <tr>
                             <th>Date</th>
                             <th>Location</th>
@@ -108,39 +240,8 @@ function Profilesection() {
                             <th>체급</th>
                             <th>결제내역</th>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>09.27</td>
-                            <td>서울</td>
-                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
-                            <td>마스터1</td>
-                            <td>블루</td>
-                            <td>-76.0kg</td>
-                            <td>결제완료</td>
-                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
-                        </tr>
-                        <tr>
-                            <td>09.27</td>
-                            <td>서울</td>
-                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
-                            <td>마스터1</td>
-                            <td>블루</td>
-                            <td>-76.0kg</td>
-                            <td>결제완료</td>
-                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
-                        </tr>
-                        <tr>
-                            <td>09.27</td>
-                            <td>서울</td>
-                            <td>예거스챔피언쉽 로컬대회 송도 오픈</td>
-                            <td>마스터1</td>
-                            <td>블루</td>
-                            <td>-76.0kg</td>
-                            <td>결제완료</td>
-                            <td className='payInfo'><button id='payInfoBtn'>상세정보</button></td>
-                        </tr>
-                    </tbody>
+                    </thead>               
+                    {renderLastCompetition()}
                 </table>
             </div>
         </section>
