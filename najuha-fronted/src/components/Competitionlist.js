0e06eee37261abc3ffb1aa1994956863b4bcc0dc
okhan1998 <whwlgns111@gmail.com>
2022-10-15 22:18:26 +0900

    feat: 검색기능 구현 완료. 단, 날짜초기화 기능을 추가로 넣어줘야함

--- najuha-fronted/src/components/Competitionlist.js
+++ najuha-fronted/src/components/Competitionlist.js
@@ -2,6 +2,16 @@ import axios from 'axios'
 import React, {useState, useEffect, useRef} from 'react'
 import './competitionlist.css'
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+
 function Competitionlist() {
     const [competitions, setCompetitions] = useState([])
     const [week, setWeek] = useState(['일', '월', '화', '수', '목', '금', '토'])
@@ -11,6 +21,7 @@ function Competitionlist() {
     const [startDate, setStartDate] = useState('');
     const [location, setLocation] = useState('');
     const [title, setTitle] = useState('')
+    const [temTitle, setTemTitle] = useState('')
     const offsetRef = useRef();
     const locationRef = useRef();
     const startDateRef = useRef();
@@ -19,10 +30,12 @@ function Competitionlist() {
     locationRef.current = location;
     startDateRef.current = startDate;
     titleRef.current = title;
+    const [locationSample, setLocationSample]=useState(['강원', '경기', '경남', '경북', '광주', '대구', '대전', '부산', '서울', '울산', '인천', '전남', '전북', '제주', '충남', '충북'])
     
     const observer = useRef(new IntersectionObserver(async (entries)=>{
         const first = entries[0]
         if(first.isIntersecting){
+            console.log('관측됨')
             await getCompetitionList(startDateRef.current, offsetRef.current, titleRef.current, locationRef.current);
             await setOffset((preOffset) =>{return preOffset+1})
         }
@@ -59,13 +72,12 @@ function Competitionlist() {
             currentObserver.observe(currentElement);
             
         }
-
         return () => {
             if (currentElement){
                 currentObserver.unobserve(currentElement)
             }
         }
-    }, [lastElement])
+    }, [lastElement, location, startDate, title])
 
     useEffect(() => {
         console.log(competitions.length)
@@ -76,31 +88,22 @@ function Competitionlist() {
     }, [offset])
 
     useEffect(() => {
+        console.log(`startDate값은: ${startDate}`)
+    }, [startDate])
+
+    useEffect(() => {
         console.log(`location값은: ${location}`)
     }, [location])
 
+    useEffect(() => {
+        console.log(`title값은: ${title}`)
+    }, [title])
 
+    function listRefresh(){ // 검색 변수가 바뀔때마다 초기화 해주어야함.
+        setOffset(0)
+        setCompetitions([]);
+    }
 
-    // useEffect(() => {
-    //     async function getCompetitionList(){
-    //         axios.get(`${process.env.REACT_APP_BACK_END_API}/competitions`,
-    //         {
-    //             headers: {
-    //                 'x-access-token':  process.env.REACT_APP_BACK_END_TOKEN
-    //             }
-    //         })
-    //         .then((res) => {
-    //             setCompetitions(res.data.result)
-    //             console.log('성공')
-    //         })
-    //         .catch((err) => {
-    //             console.log(err)
-    //             console.log(err.response.status);
-    //         })
-    //         return ;
-    //     }
-    //     getCompetitionList()
-    // }, [])
 
     function competitionParsing(competition){
         let doreOpenDay = week[new Date(competition.doreOpen).getDay()]
@@ -158,9 +161,81 @@ function Competitionlist() {
 
   return (
     <div className='competition-schedule-wrapper'>
-        <div className='searchzone'>
-        searchzone
-        <button value='서울' onClick={(e)=>setLocation(e.target.value)}>서울</button>
+        <div className='competition-searchzone'>
+            <div className='competition-searchzone-wrapper'>
+                <div className='competition-searchzone-title'>대회일정</div>
+                <div className='competition-searchzone-fake'>
+                    <img className='competition-searchzone-input-icon' src='Assets/검색돋보기아이콘.svg' alt="돋보기아이콘" onClick={()=>{
+                        setTitle(temTitle);
+                        listRefresh()
+                    }}/>
+                    <input className='competition-searchzone-input' value={temTitle} placeholder='대회 이름 직접 검색하기' onChange={(e)=> setTemTitle(e.target.value)}/>
+                </div>
+                <div className='competition-searchzone-options'>
+                    <div className='competition-searchzone-options-date'>
+                    <LocalizationProvider dateAdapter={AdapterDayjs} >
+                    <DesktopDatePicker
+                    label="날짜 선택"
+                    views={['year', 'month', 'day']}
+                    minDate={dayjs('2022-1-1')} // 올해로 한정될수 있게 변수값을 고쳐야함 
+                    maxDate={dayjs('2022-12-31')} // 올해로 한정될수 있게 변수값을 고쳐야함 
+                    inputFormat="MM-DD-YYYY" 
+                    value={startDate}
+                    onChange={(newvalue)=>{
+                        if(newvalue.format('YYYY-MM-DD') != 'Invalid Date'){
+                            setStartDate(newvalue.format('YYYY-MM-DD'))
+                            listRefresh();
+                        }
+                    }}
+                    renderInput={(params) => <TextField style={{width: '100%'}} {...params} />}
+                    />
+                    </LocalizationProvider>
+                    </div>
+                    <div className='competition-searchzone-options-location'>
+                        <FormControl variant="standard" style={{width:'100%'}}>
+                            <InputLabel id="demo-simple-select-standard-label">지역</InputLabel>
+                            <Select
+                            labelId="demo-simple-select-standard-label"
+                            id="demo-simple-select-standard"
+                            value={location}
+                            onChange={(e) => {
+                                setLocation(e.target.value)
+                                listRefresh();
+                            }}
+                            label="지역"
+                            >
+                            <MenuItem value="">
+                                <em>지역</em>
+                            </MenuItem>
+                            {locationSample.map((sample) => {
+                                return(
+                                    <MenuItem value={sample}>{sample}</MenuItem>
+                                )
+                            })}
+                            </Select>
+                        </FormControl>
+                    </div>
+                    {/* <div className='competition-searchzone-options-city'>
+                        <FormControl variant="standard" style={{width:'100%'}}>
+                        <InputLabel id="demo-simple-select-standard-label">도시</InputLabel>
+                        <Select
+                        labelId="demo-simple-select-standard-label"
+                        id="demo-simple-select-standard"
+                        value={city}
+                        onChange={(e) => {setCity}}
+                        label="Age"
+                        >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                    </div> */}
+                </div>
+            </div>
         </div>
         <div className='competition-list'>
             <ul class='competition-row'>
