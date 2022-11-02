f08b103f9c10de8a13f74167a308d485f762ad55
yeonah98 <dbdusdkaa@naver.com>
2022-11-02 21:51:17 +0900

    Fix: phoneNumber input 타입 number->text

--- najuha-fronted/src/components/UserInfo.js
+++ najuha-fronted/src/components/UserInfo.js
@@ -133,13 +133,13 @@ function  UserInfo() {
     
         const handleChange = (e, title) => {
             if(title=='phoneNumber' || title=='weight') {
-                e.target.value.replace(/[^0-9]/g, "");
                 if(title=='phoneNumber' && e.target.value.length > 11){
                     e.value = e.value.slice(0, 11)
                 }
                 if(title=='weight' && e.target.value.length > 3){
                     e.value = e.value.slice(0, 3)
                 }
+                e.target.value.replace(/[^0-9]/g, "");
             }
             console.log(userInfos);
             let newuserInfos = {...userInfos};
@@ -181,7 +181,7 @@ function  UserInfo() {
                 <div className='UserInfo_infoBox'>
                     <span>휴대폰</span>
                     <div className='UserInfo_flexbox UserInfo_phoneNumber'>
-                        <p><input type='number' name='phoneNumber' placeholder={userInfos.phoneNumber} value={userInfos.phoneNumber} onChange={(e)=>handleChange(e, 'phoneNumber')} required/></p>
+                        <p><input type='text' name='phoneNumber' placeholder={userInfos.phoneNumber} value={userInfos.phoneNumber} onChange={(e)=>handleChange(e, 'phoneNumber')} required/></p>
                         <span>'-' 없이 숫자만 입력(12자리)</span>
                     </div>
                 </div>
