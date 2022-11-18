ee5a79a381a86d812839881e13d025ff11ee523e
okhan1998 <whwlgns111@gmail.com>
2022-11-18 19:19:12 +0900

    feat: competitionApplyForm 실제 대회로 ui구현 다음할일은 fillteredcompetition을  이용할수 있도록 각선택에 onClick을 구현하면 됨

--- najuha-fronted/src/components/CompetitionApplyForm.js
+++ najuha-fronted/src/components/CompetitionApplyForm.js
@@ -1,21 +1,180 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './competitionApplyForm.css'
 import reseticon from '../src_assets/리셋아이콘.svg'
 import complete from '../src_assets/완료아이콘.svg'
 import notcomplete from '../src_assets/미완료아이콘.svg'
+import axios from 'axios';
+import { Cookies } from 'react-cookie';
+import {useParams} from 'react-router-dom';
 
 function CompetitionApplyForm() {
-    // const [isMyself, setIsMyself] = useState('');
-    // const [playerName, setPlayerName] = useState('');
-    // const [playerBirth, setPlayerBirth] = useState('');
-    // const [phoneNumber, setPhoneNumber] = useState('');   
-    // const [uniform, setUniform] = useState('');
-    // const [divisionName, setDivisionName] = useState('');
-    // const [gender, setGender] = useState('');
-    // const [belt, setBelt] = useState('');
-    // const [isMyself, setIsMyself] = useState('');
-    // const [isMyself, setIsMyself] = useState('');
-    // const [isMyself, setIsMyself] = useState('');
+    const {id} = useParams();
+    const [isMyself, setIsMyself] = useState('');
+    const [playerName, setPlayerName] = useState('');
+    const [playerBirth, setPlayerBirth] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');   
+    const [uniform, setUniform] = useState('');
+    const [divisionName, setDivisionName] = useState('');
+    const [gender, setGender] = useState('');
+    const [belt, setBelt] = useState('');
+    const [weight, setWeight] = useState('');
+    const [team, setTeam] = useState('');
+    const [competitionId, setCompetitionId] = useState('');
+    const [price, setPrice] = useState('');
+    const [competition, setCompetition] = useState(null);
+    const cookies = new Cookies();
+    const [fillteredcompetition, setFillteredCompetition] = useState(null);
+    const [competitionApplicationList, setCompetitionApplicationList] = useState(
+        [{
+            isMyself: '',
+            playerName: "",
+            playerBirth: "",
+            phoneNumber: "",
+            uniform: 'gi',
+            divisionName: '일반부',
+            gender: 'female',
+            belt: null,
+            weight: '-50',
+            team: "김포 골든라이언",
+            competitionId: '',
+            price: '',
+            check: 0, 
+        },
+    ])
+
+
+    const getCompetition = async (id) => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_BACK_END_API}/competitions/${id}`, {
+                headers: {
+                    "x-access-token":  cookies.get("x-access-token")
+                }
+            });
+            const newCompetition = response.data.result;
+            setCompetition(newCompetition)
+            setFillteredCompetition(newCompetition.division);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        getCompetition(id);
+    }, [])
+
+    useEffect(() => {
+        console.log(competition)
+    }, [competition])
+
+    useEffect(() => {
+        console.log(fillteredcompetition)
+    }, [fillteredcompetition])
+
+    const applicationDetailUI = () => {
+        return competitionApplicationList.map((application, i) => {
+            return(
+                <ul className='CompetitionApplyForm-top-table-item'>
+                    <li>{application.uniform}</li>
+                    <li>{application.divisionName}</li>
+                    <li>{application.gender}</li>
+                    <li>{application.weight}</li>
+                    <li>{application.belt}</li>
+                    <li>{application.price}</li>
+                </ul>
+            )
+        })
+    }
+
+    // const chooseUniformOption = (application, i) => {
+
+    // }
+
+    const chooseOptionUI = (application, i) => {
+        if(application.uniform == null){
+            let comuniform = [];
+            fillteredcompetition.map((com, j) => {
+                comuniform.push(com.constantFactor.uniform);
+            })
+            comuniform = [...new Set(comuniform)];
+            return comuniform.map((el, h) => {
+                return(
+                    <li onClick={(() => chooseUniformOption(application,i))}>{el == 'gi' ? '기' : '노기'}</li>
+                )
+            })
+        } else if(application.divisionName == null){
+            let comdi = [];
+            fillteredcompetition.map((com, j) => {
+                comdi.push(com.constantFactor.divisionName);
+            })
+            comdi = [...new Set(comdi)];
+            return comdi.map((el, h) => {
+                return(
+                    <li>{el}</li>
+                )
+            })
+        } else if(application.gender == null){
+            let comgender = [];
+            fillteredcompetition.map((com, j) => {
+                comgender.push(com.constantFactor.gender);
+                
+            })
+            comgender = [...new Set(comgender)];
+            return comgender.map((el, h) => {
+                return(
+                    <li>{el}</li>
+                )
+            })
+        } else if(application.weight == null){
+            let comweight = [];
+            fillteredcompetition.map((com, j) => {
+                com.variableFactor.weight.map((wei, g) => {
+                    comweight.push(wei);
+                })
+            })
+            return comweight.map((el, h) => {
+                return(
+                    <li>{el}</li>
+                )
+            }) 
+        } else if(application.belt == null){
+            let combelt = [];
+            fillteredcompetition.map((com, j) => {
+                com.variableFactor.belt.map((bel, g) => {
+                    combelt.push(bel);
+                })
+            })
+            return combelt.map((el, h) => {
+                return(
+                    <li>{el}</li>
+                )
+            }) 
+        }
+    }
+
+    const optionUI = () => {
+        return competitionApplicationList.map((application, i) => {
+            return(
+            <>
+                <div className='CompetitionApplyForm-middle-function'>
+                    <div className='CompetitionApplyForm-middle-function-re'>
+                        <img src={reseticon} style={{cursor: 'pointer'}}/>
+                        <p>다시하기</p>
+                    </div>
+                    <div className='CompetitionApplyForm-middle-function-complete'>
+                        <img src={notcomplete} style={{cursor: 'pointer'}}/>
+                        <p>선택완료</p>
+                    </div>
+                </div>
+                <ul className='CompetitionApplyForm-middle-option'>
+                    {fillteredcompetition != null ? (application.check == 0 ? chooseOptionUI(application, i) : '') : ''}
+                </ul>
+                {/* <h2>해당 대회를 신청하고자 한다면 <br/> 선택완료를 클릭해주세요</h2> */}
+                <h2 className='CompetitionApplyForm-middle-info'>신청할 대회를 선택하세요</h2>
+            </>
+            )
+        })
+    }
+
 
   return (
     <div className='CompetitionApplyForm-wrapper'>
@@ -32,33 +191,11 @@ function CompetitionApplyForm() {
                     <li>벨트</li>
                     <li>참가비</li>
                 </ul>
-                <ul className='CompetitionApplyForm-top-table-item'>
-                    <li>노기</li>
-                    <li>일반부</li>
-                    <li>여자</li>
-                    <li>-50kg</li>
-                    <li>화이트</li>
-                    <li>50,000원</li>
-                </ul>
+                {applicationDetailUI()}
             </div>
         </div>
         <div className='CompetitionApplyForm-middle'>
-            <div className='CompetitionApplyForm-middle-function'>
-                <div className='CompetitionApplyForm-middle-function-re'>
-                    <img src={reseticon} />
-                    <p>다시하기</p>
-                </div>
-                <div className='CompetitionApplyForm-middle-function-complete'>
-                    <img src={notcomplete} />
-                    <p>선택완료</p>
-                </div>
-            </div>
-            <ul className='CompetitionApplyForm-middle-option'>
-                <li>기</li>
-                <li>노기</li>
-            </ul>
-            {/* <h2>해당 대회를 신청하고자 한다면 <br/> 선택완료를 클릭해주세요</h2> */}
-            <h2 className='CompetitionApplyForm-middle-info'>신청할 대회를 선택하세요</h2>
+            {optionUI()}
         </div>
         <div className='CompetitionApplyForm-bottom'>
             <div className='CompetitionApplyForm-bottom-sum'>
