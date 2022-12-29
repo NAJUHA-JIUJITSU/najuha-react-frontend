69548f4e027e4bf131b403c3aa370a83f7e05ec5
okhan1998 <whwlgns111@gmail.com>
2022-12-29 22:58:18 +0900

    feat: PaymentSuccess.js 결제성공페이지 pc화면 완료

--- najuha-fronted/src/components/PaymentSuccess.js
+++ najuha-fronted/src/components/PaymentSuccess.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import './paymentSuccess.css'
+import doneicon from '../src_assets/결제완료아이콘.svg'
 
 function PaymentSuccess() {
   return (
-    <div>PaymentSuccess</div>
+    <div className='PaymentSuccess'>
+        <div className='PaymentSuccess-wrapper'>
+            <img src={doneicon} alt='결제완료아이콘' />
+            <h2>결제가 정상적으로 완료되었습니다.</h2>
+            <h3>결제내역은 <span>내 프로필  >  대회신청</span> 목록에서 확인하실 수 있습니다.</h3>
+            <div className='PaymentSuccess-wrapper-button'>
+              <button>이전 화면으로</button>
+              <button id='PaymentSuccess-blue-button'>대회신청 목록보기</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
