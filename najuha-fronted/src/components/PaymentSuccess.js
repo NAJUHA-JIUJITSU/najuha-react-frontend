a8cdbb343df74db6a67d55209cbdeb05539b5f50
okhan1998 <whwlgns111@gmail.com>
2022-12-30 15:53:58 +0900

    feat: 결제성공페이지 버튼에 리다이렉트 기능 추가

--- najuha-fronted/src/components/PaymentSuccess.js
+++ najuha-fronted/src/components/PaymentSuccess.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './paymentSuccess.css'
 import doneicon from '../src_assets/결제완료아이콘.svg'
+import { useNavigate } from 'react-router-dom'
 
 function PaymentSuccess() {
+  const navigate = useNavigate();
   return (
     <div className='PaymentSuccess'>
         <div className='PaymentSuccess-wrapper'>
@@ -10,8 +12,12 @@ function PaymentSuccess() {
             <h2>결제가 정상적으로 완료되었습니다.</h2>
             <h3>결제내역은 <span>내 프로필  >  대회신청</span> 목록에서 확인하실 수 있습니다.</h3>
             <div className='PaymentSuccess-wrapper-button'>
-              <button>이전 화면으로</button>
-              <button id='PaymentSuccess-blue-button'>대회신청 목록보기</button>
+              <button onClick={() => {
+                navigate('/competition')
+              }}>이전 화면으로</button>
+              <button id='PaymentSuccess-blue-button' onClick={() => {
+                navigate('/Profilepage')
+              }}>대회신청 목록보기</button>
             </div>
         </div>
     </div>
