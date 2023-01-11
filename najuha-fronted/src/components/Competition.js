3e3c269a80a6dd3b48371c167d431441ab8d2074
okhan1998 <whwlgns111@gmail.com>
2023-01-11 17:25:32 +0900

    feat: react-mardown css파일에 커스텀 디자인 완료, 반응형까지 구현

--- najuha-fronted/src/components/Competition.js
+++ najuha-fronted/src/components/Competition.js
@@ -1,20 +1,58 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './competition.css'
 import ReactMarkdown from 'react-markdown';
 
 function Competition() {
-    const markdown = `
-# Hello, World!
+    let markdown = `
+# 참가자 명단
+## 세부 정보
+11월 25일 화요일 공개
 
-This is some **bold** text.
+---
 
-Here's a list:
+# 대진표
+## 엑셀 다운로드
+[예거스 챔피언쉽 로컬대회 송도 오픈_대진표xl](www.naver.com)
 
-- Item 1
-- Item 2
-- Item 3
-  `;
+---
 
+# 주요 사항
+## 주최
+예거스 챔피언쉽
+#### 
+## 후원
+예거스 챔피언쉽  
+####
+## 설명  
+본프로젝트는 기부합니다 어쩌구~~
+
+---
+
+# 대회 안내
+### 대회규정
+- 어드 존재 안함
+- 동점으로 끝났을 때, 서든데스 연장전 진행
+- 앱솔루트: 서든데스 방식 적용
+### 계체 안내
+- 경기 시작 전 직전 계체
+- 허용 오차 : 저울의 오차 범위 +500g 허용 (계체 실패시 즉시 실격)
+- 체급 1강도 계체를 통과해야 함
+### 도복 규정
+- 도복 색상은 흰색, 검정색, 파란색만 착용 가능
+- 도복 상의 안에 여성은 래쉬가드 착용, 남성은 착용 불가
+- 도복 팔 길이는 손목에서 5cm, 바지 길이는 복사뼈에서 5cm 이내여야 함
+### 코치(세컨) 제도
+- 선수 1명당 코치 1명 동반 입장 가능
+- 코치는 대회사에서 제공하는 코치복을 착용하고 입장
+- 영상 촬영 허용
+### 보험안내
+- 선수 1명당 코치 1명 동반 입장 가능
+- 코치는 대회사에서 제공하는 코치복을 착용하고 입장
+- 영상 촬영 허용
+
+`;
+
+    // markdown =  markdown.replace(/\n/gi, '\n &nbsp;');
 
   return (
     <div className='competition-wrapper'>
@@ -56,8 +94,7 @@ Here's a list:
             <button id='competition-top-button'>대회 신청</button> 
         </div>
         <div className='competition-bottom'>
-            <ReactMarkdown children={markdown} />
-            {/* {console.log(<ReactMarkdown source={markdown} />)} */}
+            <ReactMarkdown className='competition-bottom-markdown' children={markdown}/>
         </div>
     </div>
   )
