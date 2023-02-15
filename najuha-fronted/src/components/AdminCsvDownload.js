635ec3e87e713668e33ac3c3d627cbbad1dfcae2
okhan1998 <whwlgns111@gmail.com>
2023-02-15 22:48:56 +0900

    add AdmincompetitionCsv 기능구현 완료

--- najuha-fronted/src/components/AdminCsvDownload.js
+++ najuha-fronted/src/components/AdminCsvDownload.js
@@ -1,57 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { CSVLink } from "react-csv";
 import axios from "axios";
 import { Cookies } from "react-cookie";
 
-const backBaseUrl = process.env.REACT_APP_REDIRECT_URI
+const backBaseUrl = process.env.REACT_APP_BACK_END_API
 
 const AdminCsvDownload = () => {
   const [csvData, setCsvData] = useState([]);
-  const [competitionId, setCompetitionId] = useState("");
   const cookies = new Cookies();
   const xAccessToken = cookies.get("x-access-token");
+  const competitionId = useParams().id;
+  console.log(competitionId) 
+
+
 
   const getCsvData = async () => {
-    const res = await axios({
-      method: "get",
-      headers: {
-        "x-access-token": xAccessToken,
-      },
-      url:
-        backBaseUrl +
-        "/admin/competitions/" +
-        competitionId +
-        "/competitionApplications/csv",
-    });
-    setCsvData(res.data.result);
+
+    try{
+      const res = await axios({
+        method: "get",
+        headers: {
+          "x-access-token": xAccessToken,
+        },
+        url:
+          backBaseUrl +
+          "/admin/competitions/" +
+          competitionId +
+          "/competitionApplications/csv",
+      });
+      setCsvData(res.data.result);
+    }catch{
+      console.log('에러')
+    }
+    
+    
   };
 
-  const handleChange = (e) => {
-    setCompetitionId(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  useEffect(() => {
     getCsvData();
-  };
+  }, [])
 
   return (
     <div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="competitionId"
-            value={competitionId}
-            onChange={handleChange}
-          />
-          <button type="submit">대회 신청 목록 불러오기</button>
-        </form>
-      </div>
-      <CSVLink data={csvData} filename="applicationList.csv">
-        Download me
+      <CSVLink data={csvData} filename="applicationList.csv" >
+        <button style={{fontSize:'50px', marginLeft:'50%', transform:'translate(-50%,200%)'  }}>Download me</button>
       </CSVLink>
-      <div>{csvData}</div>
+      <div style={{fontSize:'20px', marginTop:'200px'}}>{csvData}</div>
     </div>
   );
 };
