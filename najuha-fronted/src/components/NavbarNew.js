45c6fbe0ef98f4efd67fd4bc2c22dd8a08bee3f9
yeonah98 <dbdusdkaa@naver.com>
2023-02-28 20:54:41 +0900

    Feat: Add navbarNew 프로필 드랍다운 구현

--- najuha-fronted/src/components/NavbarNew.js
+++ najuha-fronted/src/components/NavbarNew.js
@@ -1,14 +1,61 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './navbarNew.css'
+import Dropdown from './Dropdown'
 import whiteProfile from '../src_assets/whiteProfile.svg'
 import grayProfile from '../src_assets/grayProfile.svg'
+import { useCookies } from 'react-cookie'
 
 function NavbarNew() {
   const [ScrollActive, setScrollActive] = useState(false)
   const [ScrollY, setScrollY] = useState(0) // window 의 pageYOffset값을 저장
+  let [isHamburgerActive, setIsHamburgerActive] = useState(false)
+  let [isnavmenuActive, setIsNavmenuActive] = useState(false)
+  const [dropdownVisibility, setDropdownVisibility] = useState(false)
+  const restApiKey = process.env.REACT_APP_REST_API_KEY
+  const redirectUri = process.env.REACT_APP_REDIRECT_URI
+  const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${restApiKey}&redirect_uri=${redirectUri}&response_type=code`
+  const [cookies, setCookie, removeCookie] = useCookies(['x-access-token'])
 
   let navigate = useNavigate()
+  const beltDropdownRef = useRef(null)
+
+  function hamburgerClick() {
+    isHamburgerActive = !isHamburgerActive
+    setIsHamburgerActive(isHamburgerActive)
+    isnavmenuActive = !isnavmenuActive
+    setIsNavmenuActive(isnavmenuActive)
+  }
+
+  let logout = () => {
+    removeCookie('x-access-token', { path: '/' })
+    alert('로그아웃이 완료되었습니다.')
+    setDropdownVisibility(false)
+    navigate('/')
+  }
+
+  useEffect(() => {
+    console.log(cookies['x-access-token'])
+  }, [cookies['x-access-token']])
+
+  //외부 클릭시 드랍다운 닫히기
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (
+        dropdownVisibility &&
+        beltDropdownRef.current &&
+        !beltDropdownRef.current.contains(event.target)
+      ) {
+        setDropdownVisibility(false)
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside)
+    }
+  }, [dropdownVisibility])
 
   function handleScroll() {
     console.log('스크롤 ' + ScrollY)
@@ -34,6 +81,31 @@ function NavbarNew() {
           : { backgroundColor: '', border: 'none' }
       }>
       <div className="MainScroll_nav">
+        <div
+          class={`MainScroll_hamburger ${isHamburgerActive ? 'active' : ''}`}
+          onClick={hamburgerClick}>
+          <span
+            className="MainScroll_bar"
+            style={
+              ScrollY > 4100
+                ? { backgroundColor: '#888888' }
+                : { backgroundColor: 'white' }
+            }></span>
+          <span
+            className="MainScroll_bar"
+            style={
+              ScrollY > 4100
+                ? { backgroundColor: '#888888' }
+                : { backgroundColor: 'white' }
+            }></span>
+          <span
+            className="MainScroll_bar"
+            style={
+              ScrollY > 4100
+                ? { backgroundColor: '#888888' }
+                : { backgroundColor: 'white' }
+            }></span>
+        </div>
         <h1
           className="MainScroll_logo"
           onClick={() => {
@@ -55,7 +127,7 @@ function NavbarNew() {
             </li>
             <li
               onClick={() => {
-                alert('세미나 일정은 아직 준비중입니다.')
+                alert('세미나 일정은 준비중입니다.')
               }}>
               세미나
             </li>
@@ -64,7 +136,40 @@ function NavbarNew() {
           <img
             class="MainScroll_profile"
             alt="벨트모양 로그인 아이콘"
+            ref={beltDropdownRef}
+            onClick={() => {
+              setDropdownVisibility(!dropdownVisibility)
+              console.log(dropdownVisibility)
+            }}
             src={ScrollY > 4100 ? grayProfile : whiteProfile}></img>
+          <div id="profile-dropdown">
+            <Dropdown visibility={dropdownVisibility}>
+              <ul>
+                <li
+                  onClick={() => {
+                    navigate('/Profilepage', { state: 'UserInfoToggle' })
+                  }}>
+                  내 프로필
+                </li>
+                <li
+                  onClick={() => {
+                    navigate('/Profilepage', { state: 'UserApplicationList' })
+                  }}>
+                  신청대회 목록
+                </li>
+                {cookies['x-access-token'] == undefined ? (
+                  <li
+                    onClick={() => {
+                      window.location.href = kakaoAuthURL
+                    }}>
+                    로그인 하기
+                  </li>
+                ) : (
+                  <li onClick={logout}>로그아웃 하기</li>
+                )}
+              </ul>
+            </Dropdown>
+          </div>
         </div>
       </div>
     </div>
