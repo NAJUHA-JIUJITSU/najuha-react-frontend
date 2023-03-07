d7221c7377a61aa34836f5c51a53e1704a83d7bb
okhan1998 <whwlgns111@gmail.com>
2023-03-07 21:02:28 +0900

    refactor: 대회리스트조회(3.1)api axios분리

---
+++ najuha-fronted/src/apis/api/competition.js
@@ -0,0 +1,39 @@
+import { axiosApi, axiosApiWithToken } from '../utils/axios'
+
+export const getCompetitionList = async (
+  startDate,
+  offset,
+  title,
+  location
+) => {
+  try {
+    const res = await axiosApi(
+      `/competitions?startDate=${startDate}&offset=${offset}&title=${title}&location=${location}`,
+      'GET'
+    )
+    console.log(res)
+    return res
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const getCompetitionDetail = async id => {
+  try {
+    const res = await axiosApi(`/competitions/${id}`, 'GET')
+    console.log(res)
+    return res
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const getCompetitionPricePredict = async id => {
+  try {
+    const res = await axiosApi(`/competitions/${id}/price`, 'GET')
+    console.log(res)
+    return res
+  } catch (e) {
+    console.log(e)
+  }
+}
