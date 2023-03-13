4c9a90c35202efcaecf6077967f72bd0dd050ec0
okhan1998 <whwlgns111@gmail.com>
2023-03-13 21:09:45 +0900

    feat: error handle함수 수정

--- najuha-fronted/src/apis/utils/error.js
+++ najuha-fronted/src/apis/utils/error.js
@@ -1,5 +1,5 @@
 export const handleError = error => {
-  switch (error.status) {
+  switch (error.response.status) {
     case 400:
       alert(`${error.message}`)
       break
@@ -27,6 +27,6 @@ export const handleError = error => {
 //400 잘못된 요청 => alert message
 //401 로그인 안됨 => alert 접근권한이 없습니다.
 //403 (대회 활성할떄 만쓰이는 듯) alert띄우기 => alert message
-//404 없는 페이지 => navigate to 404 page
+//404 없는 페이지 => navigate to 404 page => 요청하신 url에 대한 페이지를 찾을 수 없습니다.
 //409 이미 존재하는 대회 신청했을때, 업데이트할떄, => alert message
-//500 서버 에러 => navigate to 500 page
+//500 서버 에러 => navigate to 500 page => 불편을 드려 죄송합니다. 서버 에러가 발생하여 서비스 이용이 불가능합니다. 고객센터로 문의주시나 잠시 후 다시 시도해 주세요.
