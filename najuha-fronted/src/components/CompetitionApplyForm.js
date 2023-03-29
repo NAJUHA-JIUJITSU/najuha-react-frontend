de99d7d23b5f670790698f3a335b65ec4fd334e6
okhan1998 <whwlgns111@gmail.com>
2023-03-29 15:37:35 +0900

    fix: 개인신청수정에서 옵션선택 && 참가자명단에 있는 female male 여자 남자로 통일

--- najuha-fronted/src/components/CompetitionApplyForm.js
+++ najuha-fronted/src/components/CompetitionApplyForm.js
@@ -229,7 +229,13 @@ function CompetitionApplyForm() {
         <ul className="CompetitionApplyForm-top-table-item" key={i}>
           <li>{application.uniform}</li>
           <li>{application.divisionName}</li>
-          <li>{application.gender}</li>
+          <li>
+            {application.gender === null
+              ? ''
+              : application.gender === 'male'
+              ? '남자'
+              : '여자'}
+          </li>
           <li>{application.belt}</li>
           <li>{application.weight}</li>
           <li>{application.price}</li>
@@ -364,7 +370,7 @@ function CompetitionApplyForm() {
       return comgender.map((el, h) => {
         return (
           <li key={h} onClick={() => chooseGenderOption(el, i)}>
-            {el}
+            {el === 'male' ? '남자' : '여자'}
           </li>
         )
       })
